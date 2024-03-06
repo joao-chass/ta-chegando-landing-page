4c49eeae588c53b116aed6da9a8d0cc9fce5d1fc
joao B <joaoborges07@hotmail.com>
2024-03-05 23:12:06 -0300

    Section Explore

--- src/app/components/AboutSection/AboutSection.tsx
+++ src/app/components/AboutSection/AboutSection.tsx
@@ -1,26 +1,79 @@
-import React from 'react'
-import style from './AboutSection.module.css'
-import Card from '@/app/shared/Card/Card'
+import React from "react";
+import style from "./AboutSection.module.css";
+import Card from "@/app/shared/Card/Card";
+import {NewspaperIcon, MapPinIcon, StarIcon,  BellAlertIcon  } from '@heroicons/react/24/outline'
+
+import img from "../../../../imagens/homeMobile.png";
+import Image from "next/image";
 
 function AboutSection() {
   return (
     <>
-    <div className={style.container}>
-      
-            <div className={style.fontInfo }>Ajudamos pessoas na sua mobilidade pela cidade de maneira eficiente e econômica.</div>
-       
-            <div className={style.containerCards}>
-                <Card titulo={'Informativos sobre o transporte publico'} subTitulo={'Mantenha-se atualizado com informações em tempo real, facilitando sua jornada na cidade'}/>
-                <Card titulo={'Localizaçao em tempo real'} subTitulo={'Obtenha informações em tempo real sobre o seu ônibus através do mapa. Tenha acesso instantâneo a atualizações precisas que o manterão informado sobre a localização e status do seu transporte, proporcionando uma experiência mais conveniente e eficiente'}/>
-                <Card titulo={'Criação de alertas'} subTitulo={'Desfrute da conveniência de saber o horário certo para sair de casa. Com a criação de alertas, você ficará informado sobre o momento exato da chegada do seu ônibus no ponto de embarque, proporcionando uma experiência de viagem mais organizada e eficaz.'}/>
+      <div className={style.container}>
+        <div className={style.fontInfo}>
+          Explore os Recursos do{" "}
+          <span className={style.destaque}>Tá Chegando</span>!
+        </div>
+
+        <div className={style.containerCards}>
+          <div className={style.exploreInfo}>
+            <div className={style.card}>
+            <NewspaperIcon/>
             </div>
+            <b className={style.exploreTitulo}>Informativos</b>
+            <h1 className={style.exploreSubtitulo}>
+            Mantenha-se atualizado com informações em tempo real, facilitando sua jornada na cidade
+            </h1>
 
-       
-    </div>
+            <br />
+            <br />
 
-    
-      </>
-  )
+            <div className={style.card}>
+              <MapPinIcon />
+            </div>
+            <b className={style.exploreTitulo}>Localizaçao de onibus</b>
+            <h1 className={style.exploreSubtitulo}>
+            Obtenha informações em tempo real sobre o seu ônibus através do mapa. Tenha acesso instantâneo a atualizações precisas que o manterão informado sobre a localização e status do seu transporte, proporcionando uma experiência mais conveniente e eficiente
+            </h1>
+          </div>
+
+          <Image
+            src={img}
+            width="306"
+            height="560"
+            alt="Picture of the author"
+          />
+
+         
+          <div className={style.exploreInfoEnd}>
+
+          <div className={style.card}>
+          <StarIcon />
+          </div>
+            <b className={style.exploreTitulo}>Favoritos</b>
+            <h1 className={style.exploreSubtitulo}>
+            Salve os ônibus que você utiliza com frequência para facilitar suas pesquisas sempre que precisar.
+            </h1>
+
+
+            <br />
+            <br />
+
+            <div className={style.card}>
+             <BellAlertIcon />
+            </div>
+            <b className={style.exploreTitulo}>Criação de alertas</b>
+            <h1 className={style.exploreSubtitulo}>
+            Desfrute da conveniência de saber o horário certo para sair de casa. Com a criação de alertas, você ficará informado sobre o momento exato da chegada do seu ônibus no ponto de embarque, proporcionando uma experiência de viagem mais organizada e eficaz.
+            </h1>
+
+
+           
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default AboutSection
+export default AboutSection;
--- src/app/components/Home/HomeSection.tsx
+++ src/app/components/Home/HomeSection.tsx
@@ -7,56 +7,52 @@ import { Parallax } from "react-scroll-parallax";
 import googlePlay from "../../../../imagens/googlePlay.png";
 import appleStore from "../../../../imagens/appleStore.png";
 
-
 function HomeSection() {
   return (
     <div className={style.container}>
       <div className={[style.textInfoContainer, style.slideInLeft].join(" ")}>
-        <Parallax speed={-15}>
-          <h1 className={style.textInfo}>
-            Viva a nova era da{" "}
-            <span className={style.destaque}>mobilidade </span> em{" "}
-            <span className={style.destaque}>São Paulo</span>!
-          </h1>
+        <h1 className={style.textInfo}>
+          Viva a nova era da <span className={style.destaque}>mobilidade </span>{" "}
+          em <span className={style.destaque}>São Paulo</span>!
+        </h1>
 
-          <p className={style.paragrafoInfo}>
-            Navegue pela cidade com estilo e praticidade. Nosso app vai mudar
-            sua forma de se mover.
-            <br />
-            <b>Junte-se à revolução agora!</b>
-          </p>
+        <p className={style.paragrafoInfo}>
+          Navegue pela cidade com estilo e praticidade. Nosso app vai mudar sua
+          forma de se mover.
+          <br />
+          <b>Junte-se à revolução agora!</b>
+        </p>
 
-          <div className={style.lojasApp}>
-          <Image
-            src={googlePlay}
-            width="230"
-            height="85"
-            alt="Picture of the author"
-          />
-
-          <Image  
-            src={appleStore}
-            width="230"
-            height="85"
-    
-        
-            alt="Picture of the author"
-          />
+        <div className={style.lojasApp}>
+          <div className="cursor-pointer">
+            <Image
+              src={googlePlay}
+              width="230"
+              height="85"
+              alt="Picture of the author"
+            />
           </div>
-        </Parallax>
+
+          <div className="cursor-pointer">
+            <Image
+              src={appleStore}
+              width="230"
+              height="85"
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
       </div>
 
       <div className={style.rollInBlurredLeftt}>
-        <Parallax speed={-10}>
-          <Image
-            src={img}
-            width="0"
-            height="0"
-            sizes="80vw"
-            className="w-full h-auto"
-            alt="Picture of the author"
-          />
-        </Parallax>
+        <Image
+          src={img}
+          width="0"
+          height="0"
+          sizes="80vw"
+          className="w-full h-auto"
+          alt="Picture of the author"
+        />
       </div>
     </div>
   );
